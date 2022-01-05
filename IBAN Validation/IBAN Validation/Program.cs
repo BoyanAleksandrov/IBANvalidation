3a0297fd6abe84993983f66168d14f36ee8da5eb
Boyan A <80381466+BoyanAleksandrov@users.noreply.github.com>
2022-01-05 21:23:09 +0200

    Add files via upload

---
+++ IBAN Validation/IBAN Validation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace IBAN_Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iban = Console.ReadLine();
+            Console.WriteLine(checkIban(iban));
+
+           
+
+        }
+        static bool checkIban(string iban) {
+
+           
+
+            string pattern = "^BG[0-9A-Z]{20}$";
+
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(iban);
+
+            if (!match.Success)
+            {
+                return false;
+            }
+
+            String bank = iban.Substring(4) + iban.Substring(0, 4);
+            StringBuilder sb = new StringBuilder();
+            foreach (char sym in iban)
+            {
+                if (char.IsDigit(sym))
+                {
+                    sb.Append(sym);
+                }
+                else
+                {
+
+                    sb.Append(sym - 'A' + 10);
+                }
+            }
+            if (true)
+            {
+
+            }
+            return true;
+        }
+    }
+
+    
+
+
+
+
+    }
+}
